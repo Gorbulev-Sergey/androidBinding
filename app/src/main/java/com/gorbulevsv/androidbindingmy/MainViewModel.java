db14c606d51f286a3148498b7a1cd4a70c74d1d6
gorbulevsv <gorbulevsv@yandex.ru>
2022-05-01 00:52:31 +0300

    1

--- app/src/main/java/com/gorbulevsv/androidbindingmy/MainViewModel.java
+++ app/src/main/java/com/gorbulevsv/androidbindingmy/MainViewModel.java
@@ -9,6 +9,7 @@ import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
     public MutableLiveData<String> title = new MutableLiveData<>("Привет");
+    public MutableLiveData<String> text = new MutableLiveData<>("девушку");
 
     public void buttonOnClick(View view) {
         title.setValue(title.getValue() + " 10");
